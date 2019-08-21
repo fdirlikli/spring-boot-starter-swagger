47388fc0960369febce7574688ed3ca2cb812a48
Fatih Dirlikli <fatihdirlikli@gmail.com>
2019-08-20 21:34:42 -0700

    Initial commit

---
+++ src/main/java/org/orwashere/swagger/SpringBootStarterSwaggerApplication.java
@@ -0,0 +1,13 @@
+package org.orwashere.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterSwaggerApplication.class, args);
+	}
+
+}
