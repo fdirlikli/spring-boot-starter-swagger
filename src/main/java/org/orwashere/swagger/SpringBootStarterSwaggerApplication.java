3974da0f9176f1103088a96923dfa31a45ffafe7
Fatih Dirlikli <fatihdirlikli@gmail.com>
2019-08-20 22:55:19 -0700

    Adding swagger dependencies.

--- src/main/java/org/orwashere/swagger/SpringBootStarterSwaggerApplication.java
+++ src/main/java/org/orwashere/swagger/SpringBootStarterSwaggerApplication.java
@@ -1,13 +1,65 @@
 package org.orwashere.swagger;
 
+import com.fasterxml.classmate.TypeResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootStarterSwaggerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStarterSwaggerApplication.class, args);
 	}
 
+	@Bean
+	public Docket petApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build()
+				.pathMapping("/")
+				.directModelSubstitute(LocalDate.class, String.class)
+				.genericModelSubstitutes(ResponseEntity.class)
+//				.alternateTypeRules(
+//						newRule(typeResolver.resolve(DeferredResult.class,
+//								typeResolver.resolve(ResponseEntity.class, WildcardType.class)),
+//								typeResolver.resolve(WildcardType.class)))
+				.useDefaultResponseMessages(false)
+//				.globalResponseMessage(RequestMethod.GET,
+//						newArrayList(new ResponseMessageBuilder()
+//								.code(500)
+//								.message("500 message")
+//								.responseModel(new ModelRef("Error"))
+//								.build()))
+//				.securitySchemes(newArrayList(apiKey()))
+//				.securityContexts(newArrayList(securityContext()))
+//				.enableUrlTemplating(true)
+//				.globalOperationParameters(
+//						newArrayList(new ParameterBuilder()
+//								.name("someGlobalParameter")
+//								.description("Description of someGlobalParameter")
+//								.modelRef(new ModelRef("string"))
+//								.parameterType("query")
+//								.required(true)
+//								.build()))
+//				.tags(new Ta
+//						g("Pet Service", "All apis relating to pets"))
+//				.additionalModels(typeResolver.resolve(AdditionalModel.class))
+				;
+	}
+
+	@Autowired
+	private TypeResolver typeResolver;
 }
